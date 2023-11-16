a26462743118ac83beef21858e3b336d934f9c4e
bitha25 <dumbrava_gabriel@yahoo.com>
2023-11-16 18:57:14 +0200

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movies, setMovies] = useState();
+    const getMovies = async () => {
+        try {
+            const response = await axios.get("http://localhost:8080/api/v1/movies")
+            setMovies(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
 
 export default App;
